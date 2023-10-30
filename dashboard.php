38bb6e880004b6b58b7d6f91f667eacd7ddbb35a
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2023-10-30 21:23:09 +0800

    Assets Images

--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,37 @@
     <link rel="stylesheet" href="./styles.css">
 </head>
 <body>
-    
+    <div class="ticket-visual_visual" id="ticket">
+        <div class="left"></div>
+        <div class="right"></div>
+        <div class="ticket-visual-wrapper">
+            <div class="ticket-visual_profile">
+            <div class="ticket-profile_profile">
+                <img
+                src="https://github.com/madflows.png"
+                alt="madflows"
+                class="ticket-profile_image"
+                />
+                <div class="ticket-profile_text">
+                <p class="ticket-profile_name">Folarin Lawal</p>
+                <p class="ticket-profile_username">
+                    <span class="ticket-profile_githubIcon">
+                    <img src="./github.svg" alt="" />
+                    </span>
+                    madflows
+                </p>
+                <!-- Refactored html -->
+                </div>
+            </div>
+            <div class="ticket-event">
+            <img src="./event-logos.png" />
+            </div>
+            <div class="ticket-visual_ticket-number-wrapper">
+            <div class="ticket-visual_ticket-number">№ 014747</div>
+            </div>
+        </div>
+        </div>
+
+        <script src="./script.js"></script>
 </body>
 </html>
