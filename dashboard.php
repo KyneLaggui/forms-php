cbf08e665872729ff485352d3ea68dd4fad3bf95
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2023-10-30 22:17:53 +0800

    Remove unecessary code unfinished

--- dashboard.php
+++ dashboard.php
@@ -4,40 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="./styles.css">
+    <link rel="stylesheet" href="./dashboard.css">
 </head>
 <body>
-    <div class="ticket-visual_visual" id="ticket">
-        <div class="left"></div>
-        <div class="right"></div>
-        <div class="ticket-visual-wrapper">
-            <div class="ticket-visual_profile">
-            <div class="ticket-profile_profile">
-                <img
-                src="https://github.com/madflows.png"
-                alt="madflows"
-                class="ticket-profile_image"
-                />
-                <div class="ticket-profile_text">
-                <p class="ticket-profile_name">Folarin Lawal</p>
-                <p class="ticket-profile_username">
-                    <span class="ticket-profile_githubIcon">
-                    <img src="./github.svg" alt="" />
-                    </span>
-                    madflows
-                </p>
-                <!-- Refactored html -->
+    <div class="ticket-main">
+        <div class="ticket-main-body" id="ticket">
+            <div class="ticket-main-wrapper">
+                <div class="ticket-top">
+                    <div class="ticket-profile-details">
+                        <img
+                        src="./Images/Profile-Picture.jpg"
+                        alt="AstronautImg"
+                        class="ticket-profile-img"
+                        />
+                        <div class="ticket-profile_text">
+                        <p class="ticket-surname">Buhain</p>
+                        <p class="ticket-profile_username">
+                            <span class="ticket-profile_githubIcon">
+                            <img src="./github.svg" alt="" />
+                            </span>
+                            madflows
+                        </p>
+                        <!-- Refactored html -->
+                        </div>
+                    </div>
+                    <div class="ticket-event">
+                        <img src="./event-logos.png" />
+                    </div>
+                    <div class="ticket-visual_ticket-number-wrapper">
+                        <div class="ticket-visual_ticket-number">№ 014747</div>
+                    </div>
                 </div>
             </div>
-            <div class="ticket-event">
-            <img src="./event-logos.png" />
-            </div>
-            <div class="ticket-visual_ticket-number-wrapper">
-            <div class="ticket-visual_ticket-number">№ 014747</div>
-            </div>
         </div>
-        </div>
+    </div>
 
-        <script src="./script.js"></script>
+
+    <script src="./main.js"></script>
+        
 </body>
 </html>
